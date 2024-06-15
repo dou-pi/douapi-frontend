f8bd119871590f70a5ad702b312348da223e944e
A-yong <2378786248@qq.com>
2024-06-15 17:54:52 +0800

    Complete

---
+++ src/pages/User/Login/index.tsx
@@ -0,0 +1,203 @@
+import Footer from '@/components/Footer';
+import {LockOutlined, UserOutlined,} from '@ant-design/icons';
+import {LoginForm, ProFormCheckbox, ProFormText,} from '@ant-design/pro-components';
+import {history, useModel} from '@umijs/max';
+import {Alert, message, Tabs} from 'antd';
+import React, {useState} from 'react';
+import styles from './index.less';
+import {userLoginUsingPOST} from '@/services/douapi-backend/userController';
+import { Link } from 'react-router-dom';
+
+const LoginMessage: React.FC<{
+  content: string;
+}> = ({content}) => {
+  return (
+    <Alert
+      style={{
+        marginBottom: 24,
+      }}
+      message={content}
+      type="error"
+      showIcon
+    />
+  );
+};
+const Login: React.FC = () => {
+  const [userLoginState, setUserLoginState] = useState<API.LoginResult>({});
+  const [type, setType] = useState<string>('account');
+  const {initialState, setInitialState} = useModel('@@initialState');
+  const handleSubmit = async (values: API.UserLoginRequest) => {
+    try {
+      // 登录
+      const res = await userLoginUsingPOST({
+        ...values,
+      });
+      if (res.data) {
+        const urlParams = new URL(window.location.href).searchParams;
+        // 设置一个延迟100毫秒的定时器
+        // 定时器触发后，导航到重定向URL，如果没有重定向URL，则导航到根路径
+        setTimeout(() => {
+          history.push(urlParams.get('redirect') || '/');
+        }, 100);
+        setInitialState({
+          loginUser: res.data
+        });
+        return;
+      }
+    } catch (error) {
+      const defaultLoginFailureMessage = '登录失败，请重试！';
+      console.log(error);
+      message.error(defaultLoginFailureMessage);
+    }
+  };
+  const {status, type: loginType} = userLoginState;
+  return (
+    <div className={styles.container}>
+      <div className={styles.content}>
+        <LoginForm
+          logo={<img alt="logo" src="/logo.png"/>}
+          title="豆PI"
+          subTitle={'API 开放平台'}
+          initialValues={{
+            autoLogin: true,
+          }}
+          /*actions={[
+            '其他登录方式 :',
+            <AlipayCircleOutlined key="AlipayCircleOutlined" className={styles.icon} />,
+            <TaobaoCircleOutlined key="TaobaoCircleOutlined" className={styles.icon} />,
+            <WeiboCircleOutlined key="WeiboCircleOutlined" className={styles.icon} />,
+          ]}*/
+          onFinish={async (values) => {
+            await handleSubmit(values as API.UserLoginRequest);
+          }}
+        >
+          <Tabs
+            activeKey={type}
+            onChange={setType}
+            centered
+            items={[
+              {
+                key: 'account',
+                label: '账户密码登录',
+              }
+
+            ]}
+          />
+
+          {status === 'error' && loginType === 'account' && (
+            <LoginMessage content={'错误的用户名和密码(admin/ant.design)'}/>
+          )}
+          {type === 'account' && (
+            <>
+              <ProFormText
+                name="userAccount"
+                fieldProps={{
+                  size: 'large',
+                  prefix: <UserOutlined className={styles.prefixIcon}/>,
+                }}
+                placeholder={'用户名'}
+                rules={[
+                  {
+                    required: true,
+                    message: '用户名是必填项！',
+                  },
+                ]}
+              />
+              <ProFormText.Password
+                name="userPassword"
+                fieldProps={{
+                  size: 'large',
+                  prefix: <LockOutlined className={styles.prefixIcon}/>,
+                }}
+                placeholder={'密码'}
+                rules={[
+                  {
+                    required: true,
+                    message: '密码是必填项！',
+                  },
+                ]}
+              />
+            </>
+          )}
+
+              {/*{status === 'error' && loginType === 'mobile' && <LoginMessage content="验证码错误" />}
+          {type === 'mobile' && (
+            <>
+              <ProFormText
+                fieldProps={{
+                  size: 'large',
+                  prefix: <MobileOutlined className={styles.prefixIcon} />,
+                }}
+                name="mobile"
+                placeholder={'请输入手机号！'}
+                rules={[
+                  {
+                    required: true,
+                    message: '手机号是必填项！',
+                  },
+                  {
+                    pattern: /^1\d{10}$/,
+                    message: '不合法的手机号！',
+                  },
+                ]}
+              />
+              <ProFormCaptcha
+                fieldProps={{
+                  size: 'large',
+                  prefix: <LockOutlined className={styles.prefixIcon} />,
+                }}
+                captchaProps={{
+                  size: 'large',
+                }}
+                placeholder={'请输入验证码！'}
+                captchaTextRender={(timing, count) => {
+                  if (timing) {
+                    return `${count} ${'秒后重新获取'}`;
+                  }
+                  return '获取验证码';
+                }}
+                name="captcha"
+                rules={[
+                  {
+                    required: true,
+                    message: '验证码是必填项！',
+                  },
+                ]}
+                onGetCaptcha={async (phone) => {
+                  const result = await getFakeCaptcha({
+                    phone,
+                  });
+                  if (result === false) {
+                    return;
+                  }
+                  message.success('获取验证码成功！验证码为：1234');
+                }}
+              />
+            </>
+          )}*/}
+              <div
+                style={{
+                  marginBottom: 24,
+                }}
+              >
+                <ProFormCheckbox noStyle name="autoLogin">
+                  自动登录
+                </ProFormCheckbox>
+
+                <Link to="/user/register">新用户注册</Link>
+
+                <a
+                  style={{
+                    float: 'right',
+                  }}
+                >
+                  忘记密码请联系管理员 ?
+                </a>
+              </div>
+            </LoginForm>
+            </div>
+            <Footer/>
+            </div>
+            );
+          };
+          export default Login;
